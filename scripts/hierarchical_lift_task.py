54e98c55eb6cfede8ee6126b4758b10c935c031e
MarionLepert <marion.lepert@gmail.com>
2020-10-25 15:59:35 -0700

    fixed goal_pose

--- scripts/hierarchical_lift_task.py
+++ scripts/hierarchical_lift_task.py
@@ -36,6 +36,7 @@ def main():
     )
     rl_load_dir, start_mode = '', PolicyMode.TRAJ_OPT
     initial_pose = move_cube.sample_goal(difficulty=-1)
+    goal_pose = move_cube.Pose.from_dict(goal)
     policy = HierarchicalControllerPolicy(action_space=env.action_space,
                    initial_pose=initial_pose, goal_pose=goal_pose,
                    load_dir=rl_load_dir, difficulty=difficulty,
