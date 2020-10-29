00340a862cbba4711f45401121e161b550878de9
ClaireLC <clairech@stanford.edu>
2020-10-29 16:26:01 -0700

    kv = 1

--- python/rrc_iprl_package/control/control_policy.py
+++ python/rrc_iprl_package/control/control_policy.py
@@ -34,9 +34,12 @@ KP = [25, 25, 25,
       25, 25, 25,
       25, 25, 25]
 
-KV = [0.5, 0.5, 0.5, 
-      0.5, 0.5, 0.5,
-      0.5, 0.5, 0.5]
+#KV = [0.5, 0.5, 0.5, 
+#      0.5, 0.5, 0.5,
+#      0.5, 0.5, 0.5]
+KV = [1, 1, 1, 
+      1, 1, 1,
+      1, 1, 1]
 
 class ImpedanceControllerPolicy:
     def __init__(self, action_space=None, initial_pose=None, goal_pose=None,
@@ -159,10 +162,10 @@ class ImpedanceControllerPolicy:
         for i in range(3):
             self.ft_tracking_waypoints_list[i][2] = 0.05
         self.ft_tracking_waypoints_list[0][0] = 0.0
-        self.ft_tracking_waypoints_list[0][1] = 0.1
+        self.ft_tracking_waypoints_list[0][1] = 0.08
         self.ft_tracking_waypoints_list[1][0] = 0.0
-        self.ft_tracking_waypoints_list[1][1] = -0.1
-        self.ft_tracking_waypoints_list[2][0] = -0.1
+        self.ft_tracking_waypoints_list[1][1] = -0.08
+        self.ft_tracking_waypoints_list[2][0] = -0.08
         self.ft_tracking_waypoints_list[2][1] = 0
 
         csv_header = "step,desired_ft0,desired_ft1,desired_ft2,desired_ft3,desired_ft4,desired_ft5,desired_ft6,desired_ft7,desired_ft8,"
