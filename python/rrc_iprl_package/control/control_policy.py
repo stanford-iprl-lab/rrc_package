65e39497cc1b4dcb62f2bffdb27dd2169ae857df
ClaireLC <clairech@stanford.edu>
2020-10-29 22:30:06 -0700

    track traj

--- python/rrc_iprl_package/control/control_policy.py
+++ python/rrc_iprl_package/control/control_policy.py
@@ -208,7 +208,6 @@ class ImpedanceControllerPolicy:
                 self.fingertip_goal_list = []
                 for f_i in range(3):
                     self.fingertip_goal_list.append(self.finger_waypoints_list[f_i][self.pre_traj_waypoint_i])
-                self.fingertip_goal_list = self.ft_tracking_waypoints_list
                 self.tol = 0.005
                 self.tip_forces_wf = None
 
