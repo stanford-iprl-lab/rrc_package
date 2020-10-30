879ad4ea5d9fe89afef6ffbeddae54ddc2472b73
ClaireLC <clairech@stanford.edu>
2020-10-29 22:27:43 -0700

    track traj

--- python/rrc_iprl_package/control/control_policy.py
+++ python/rrc_iprl_package/control/control_policy.py
@@ -166,7 +166,8 @@ class ImpedanceControllerPolicy:
 
         for f_i in range(3):
             for i in range(1,5):
-                self.ft_tracking_waypoints_list[f_i].append(self.ft_tracking_waypoints_list[f_i][i-1][2] - 0.01)
+                next_waypoint = self.ft_tracking_waypoints_list[f_i][i-1] - np.array([0,0,0.01])
+                self.ft_tracking_waypoints_list[f_i].append(next_waypoint)
 
         #for i in range(3):
         #    #self.ft_tracking_waypoints_list[i][2] = 0.1
