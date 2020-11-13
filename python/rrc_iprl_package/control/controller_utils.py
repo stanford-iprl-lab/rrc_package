5e2ce2225e0a4f25ed3257ef5fb2aaf01fe595e6
ClaireLC <clairech@stanford.edu>
2020-11-13 01:06:03 -0800

    add force to flip

--- python/rrc_iprl_package/control/controller_utils.py
+++ python/rrc_iprl_package/control/controller_utils.py
@@ -703,14 +703,15 @@ def get_flipping_waypoint(
                 # Convert back to wf
                 f_new_wf = get_wf_from_of(f_new_of, obj_pose)
                 
+                face = get_face_from_cp_param(cp_params[f_i])
                 f_vel_of = np.zeros(3)
                 f_vel_of[circle_x_dim] = dx
                 f_vel_of[circle_y_dim] = dy
+                f_vel_of[np.nonzero(OBJ_FACES_INFO[face]["up_axis"])[0][0]] += 0.005
                 f_vel_wf = rotate_wf_from_of(f_vel_of, obj_pose)
 
-                face = get_face_from_cp_param(cp_params[f_i])
                 f_l_of = np.zeros(3)
-                f_l_of[np.nonzero(OBJ_FACES_INFO[face]["up_axis"])[0][0]] = 1
+                #f_l_of[np.nonzero(OBJ_FACES_INFO[face]["up_axis"])[0][0]] = 1
                 print(f_l_of)
                 f_l_wf = rotate_wf_from_of(f_l_of, obj_pose)
                 print(f_l_wf)
