c97535a919284cd697d4efe8600e73285176d7d7
krishnan <krshna@stanford.edu>
2020-11-12 16:38:46 -0800

    save action log in run script

--- scripts/hierarchical_lift_task.py
+++ scripts/hierarchical_lift_task.py
@@ -7,19 +7,16 @@ dummy policy which uses random actions.
 import json
 import sys
 import os
+import os.path as osp
 import numpy as np
-from gym.wrappers import TimeLimit
 
-from rrc_iprl_package.envs import cube_env, custom_env, env_wrappers
+from rrc_iprl_package.envs import cube_env, custom_env
 from trifinger_simulation.tasks import move_cube 
 from rrc_iprl_package.control.controller_utils import PolicyMode
 from rrc_iprl_package.control.control_policy import HierarchicalControllerPolicy
-from rrc_iprl_package.envs.custom_env import ResidualPolicyWrapper
-
-import rrc_iprl_package.run_rrc_sb as sb_utils
 
 FRAMESKIP = 1
-EP_LEN = 120 * 1000 // FRAMESKIP
+#MAX_STEPS = 3 * 1000 // FRAMESKIP
 MAX_STEPS = None
 
 class RandomPolicy:
@@ -36,8 +33,6 @@ def main():
     # the difficulty level and the goal pose (as JSON string) are passed as
     # arguments
     difficulty = int(sys.argv[1])
-    initial_pose = move_cube.sample_goal(-1)
-    # initial_pose.position = np.array([0,0,.0325])
     goal_pose_json = sys.argv[2]
     if os.path.exists(goal_pose_json):
         with open(goal_pose_json) as f:
@@ -49,41 +44,46 @@ def main():
     #initial_pose.position = np.array([-0.08,-0.02,.0325])
     theta = np.pi/6
     initial_pose.orientation = np.array([0, 0, np.sin(theta/2), np.cos(theta/2)])
-
+   
+    if osp.exists('/output'):
+        save_path = '/output/action_log.npz'
+    else:
+        save_path = 'action_log.npz'
     env = cube_env.RealRobotCubeEnv(
-        goal, initial_pose.to_dict(), difficulty,
-        cube_env.ActionType.TORQUE, frameskip=FRAMESKIP,
-        num_steps=MAX_STEPS, visualization = True
+        goal, initial_pose, difficulty,
+        cube_env.ActionType.TORQUE_AND_POSITION, frameskip=FRAMESKIP,
+        num_steps=MAX_STEPS, visualization=False, save_npz=save_path
     )
-    rl_load_dir = './models/HER.zip'
-    env = custom_env.ResidualPolicyWrapper(env, goal_env=True)
-    env = TimeLimit(env, max_episode_steps=EP_LEN)
-    env = env_wrappers.FlattenGoalWrapper(env)
-    policy = sb_utils.make_model(env, None)
-    policy.load(rl_load_dir)
+    rl_load_dir, start_mode = '', PolicyMode.TRAJ_OPT
+    initial_pose = move_cube.sample_goal(difficulty=-1)
+    initial_pose.position = np.array([0,0,.0325])
+    goal_pose = move_cube.Pose.from_dict(goal)
+    policy = HierarchicalControllerPolicy(action_space=env.action_space,
+                   initial_pose=initial_pose, goal_pose=goal_pose,
+                   load_dir=rl_load_dir, difficulty=difficulty,
+                   start_mode=start_mode)
+    env = custom_env.HierarchicalPolicyWrapper(env, policy)
     observation = env.reset()
-    print("Finished reset")
 
     accumulated_reward = 0
     is_done = False
-    # old_mode = policy.mode
+    old_mode = policy.mode
     steps_so_far = 0
-    filepath = '/output/reward.csv'
-    with open(filepath, 'w') as f:
-        while not is_done:
-            if MAX_STEPS is not None and steps_so_far == MAX_STEPS: break
-            action, _ = policy.predict(observation)
-            observation, reward, is_done, info = env.step(action)
-            # if old_mode != policy.mode:
-                #print('mode changed: {} to {}'.format(old_mode, policy.mode))
-            #     old_mode = policy.mode
-            #print("reward:", reward)
-            accumulated_reward += reward
-            steps_so_far += 1
-            f.write("{},{},{}".format(steps_so_far, reward, accumulated_reward))
+    while not is_done:
+        if MAX_STEPS is not None and steps_so_far == MAX_STEPS: break
+        action = policy.predict(observation)
+        observation, reward, is_done, info = env.step(action)
+        if old_mode != policy.mode:
+            #print('mode changed: {} to {}'.format(old_mode, policy.mode))
+            old_mode = policy.mode
+        #print("reward:", reward)
+        accumulated_reward += reward
+        steps_so_far += 1
 
-    print("------")
-    print("Accumulated Reward: {:.3f}".format(accumulated_reward))
+    env.save_action_log()
+
+    #print("------")
+    #print("Accumulated Reward: {:.3f}".format(accumulated_reward))
 
 
 if __name__ == "__main__":
