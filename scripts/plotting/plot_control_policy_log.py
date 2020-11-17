124e36d49dfa15dfffec3fea226f4d9d9bf694cb
ClaireLC <clairech@stanford.edu>
2020-11-17 14:23:22 -0800

    example plotting code

--- scripts/plotting/plot_control_policy_log.py
+++ scripts/plotting/plot_control_policy_log.py
@@ -9,19 +9,21 @@ args = parser.parse_args()
 # Open npz file
 data = np.load(args.filename)
 
-step_count = data["step_count"]
-timestamp = data["timestamp"]
-desired_ft_pos = data["desired_ft_pos"]
-desired_ft_vel = data["desired_ft_vel"]
-actual_ft_pos = data["actual_ft_pos"]
-desired_obj_pos = data["desired_obj_pose"]
+# Get arrays from npz file
+step_count       = data["step_count"]
+timestamp        = data["timestamp"]
+desired_ft_pos   = data["desired_ft_pos"]
+desired_ft_vel   = data["desired_ft_vel"]
+actual_ft_pos    = data["actual_ft_pos"]
+desired_obj_pose = data["desired_obj_pose"]
 desired_ft_force = data["desired_ft_force"]
 
-print(step_count.shape)
-print(timestamp.shape)
-print(desired_ft_pos.shape)
-print(desired_ft_vel.shape)
-print(actual_ft_pos.shape)
-print(desired_obj_pos.shape)
-print(desired_ft_force.shape)
+# Plot desired object trajectory
+steps = step_count.shape[0]
+plt.figure()
+plt.plot(range(steps), desired_obj_pose[:,2], '.')
+plt.savefig("temp.png")
+
+
+
 
