b0a055635e477759ba5f90d382f86ebc413bdd98
ClaireLC <clairech@stanford.edu>
2020-11-19 21:25:24 -0800

    remove timeout

--- scripts/hierarchical_lift_task.py
+++ scripts/hierarchical_lift_task.py
@@ -16,8 +16,8 @@ from rrc_iprl_package.control.controller_utils import PolicyMode
 from rrc_iprl_package.control.control_policy import HierarchicalControllerPolicy
 
 FRAMESKIP = 1
-MAX_STEPS = 5 * 1000 // FRAMESKIP
-#MAX_STEPS = None
+#MAX_STEPS = 5 * 1000 // FRAMESKIP
+MAX_STEPS = None
 
 class RandomPolicy:
     """Dummy policy which uses random actions."""
