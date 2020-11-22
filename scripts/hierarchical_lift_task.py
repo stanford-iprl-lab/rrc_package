dd07ea47f4d41087da3408142827b36c51a54932
krishnan <krshna@stanford.edu>
2020-11-21 16:06:56 -0800

    updated initial pose key and reset waypoint counter

--- scripts/hierarchical_lift_task.py
+++ scripts/hierarchical_lift_task.py
@@ -107,7 +107,7 @@ def main():
             # reset and run the next episode 
             else:
                 observation = env.reset()
-                initial_pose = move_cube.Pose.from_dict(observation['achieved_goal'])
+                initial_pose = move_cube.Pose.from_dict(observation['impedance']['achieved_goal'])
                 policy.impedance_controller.set_init_goal(initial_pose, goal_pose)
                 is_done = False
                 steps_so_far += 1
