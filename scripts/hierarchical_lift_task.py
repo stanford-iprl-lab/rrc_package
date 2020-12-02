81aa57d098a2bac6e897ee0b06fcf8ea8576a66a
ClaireLC <clairech@stanford.edu>
2020-12-02 10:32:04 -0800

    change max_steps

--- scripts/hierarchical_lift_task.py
+++ scripts/hierarchical_lift_task.py
@@ -16,8 +16,8 @@ from rrc_iprl_package.control.controller_utils import PolicyMode
 from rrc_iprl_package.control.control_policy import HierarchicalControllerPolicy
 
 FRAMESKIP = 1
-#MAX_STEPS = 5 * 1000 // FRAMESKIP
-MAX_STEPS = None
+MAX_STEPS = 14 * 1000 // FRAMESKIP
+#MAX_STEPS = None # For running on real robot
 
 class RandomPolicy:
     """Dummy policy which uses random actions."""
@@ -41,7 +41,7 @@ def main():
         goal = json.loads(goal_pose_json)
     initial_pose = move_cube.sample_goal(-1)
     initial_pose.position = np.array([0,0,move_cube._CUBOID_SIZE[2]/2])
-    theta = 0.0
+    theta = np.pi/4
     initial_pose.orientation = np.array([0, 0, np.sin(theta/2), np.cos(theta/2)])
    
     if osp.exists('/output'):
@@ -51,7 +51,7 @@ def main():
     env = cube_env.RealRobotCubeEnv(
         goal, initial_pose.to_dict(), difficulty,
         cube_env.ActionType.TORQUE_AND_POSITION, frameskip=FRAMESKIP,
-        num_steps=MAX_STEPS, visualization=False, save_npz=save_path
+        num_steps=MAX_STEPS, visualization=True, save_npz=save_path
     )
     rl_load_dir, start_mode = '', PolicyMode.TRAJ_OPT
     goal_pose = move_cube.Pose.from_dict(goal)
