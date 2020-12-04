f0d7f1c007a74c4d692c6fa1941bcc32969be3ec
ClaireLC <clairech@stanford.edu>
2020-12-03 17:32:51 -0800

    updated cube_env

--- python/rrc_iprl_package/envs/cube_env.py
+++ python/rrc_iprl_package/envs/cube_env.py
@@ -46,13 +46,13 @@ class RealRobotCubeEnv(gym.GoalEnv):
         cube_initial_pose: dict = None,
         goal_difficulty: int = 1,
         action_type: ActionType = ActionType.POSITION,
+        default_position: np.ndarray = np.array([0.0, 0.75, -1.6] * 3),
         visualization: bool = True,
         frameskip: int = 1,
         num_steps: int = None,
         save_npz: str = None,
     ):
         """Initialize.
-
         Args:
             cube_goal_pose (dict): Goal pose for the cube.  Dictionary with
                 keys "position" and "orientation".
@@ -82,6 +82,7 @@ class RealRobotCubeEnv(gym.GoalEnv):
             raise ValueError("frameskip cannot be less than 1.")
         self.frameskip = frameskip
         self.episode_length = num_steps * frameskip if num_steps else move_cube.episode_length
+        self.num_resets = 0
 
         # will be initialized in reset()
         self.platform = None
@@ -121,12 +122,15 @@ class RealRobotCubeEnv(gym.GoalEnv):
         if not object_state_space.contains(self.goal):
             raise ValueError("Invalid goal pose.")
 
+        self.default_position = default_position
         if self.action_type == ActionType.TORQUE:
+            assert self.action_type in [ActionType.POSITION,
+                    ActionType.TORQUE_AND_POSITION]
             self.action_space = robot_torque_space
             self._initial_action = trifingerpro_limits.robot_torque.default
         elif self.action_type == ActionType.POSITION:
             self.action_space = robot_position_space
-            self._initial_action = trifingerpro_limits.robot_position.default
+            self._initial_action = default_position
         elif self.action_type == ActionType.TORQUE_AND_POSITION:
             self.action_space = gym.spaces.Dict(
                 {
@@ -136,7 +140,7 @@ class RealRobotCubeEnv(gym.GoalEnv):
             )
             self._initial_action = {
                 "torque": trifingerpro_limits.robot_torque.default,
-                "position": trifingerpro_limits.robot_position.default,
+                "position": default_position,
             }
         else:
             raise ValueError("Invalid action_type")
@@ -153,8 +157,6 @@ class RealRobotCubeEnv(gym.GoalEnv):
                 "action": self.action_space,
                 "desired_goal": object_state_space,
                 "achieved_goal": object_state_space,
-                #"cam0_timestamp": gym.spaces.Box(low=0., high=np.inf, shape=())
-                #"achieved_goal_filtered": object_state_space,
             }
         )
         self.save_npz = save_npz
@@ -162,18 +164,15 @@ class RealRobotCubeEnv(gym.GoalEnv):
 
     def compute_reward(self, achieved_goal, desired_goal, info):
         """Compute the reward for the given achieved and desired goal.
-
         Args:
             achieved_goal (dict): Current pose of the object.
             desired_goal (dict): Goal pose of the object.
             info (dict): An info dictionary containing a field "difficulty"
                 which specifies the difficulty level.
-
         Returns:
             float: The reward that corresponds to the provided achieved goal
             w.r.t. to the desired goal. Note that the following should always
             hold true::
-
                 ob, reward, done, info = env.step()
                 assert reward == env.compute_reward(
                     ob['achieved_goal'],
@@ -189,17 +188,13 @@ class RealRobotCubeEnv(gym.GoalEnv):
 
     def step(self, action):
         """Run one timestep of the environment's dynamics.
-
         When end of episode is reached, you are responsible for calling
         ``reset()`` to reset this environment's state.
-
         Args:
             action: An action provided by the agent (depends on the selected
                 :class:`ActionType`).
-
         Returns:
             tuple:
-
             - observation (dict): agent's observation of the current
               environment.
             - reward (float) : amount of reward returned after previous action.
@@ -243,8 +238,9 @@ class RealRobotCubeEnv(gym.GoalEnv):
             if self.step_count >= self.episode_length:
                 break
 
-        is_done = self.step_count == self.episode_length
+        is_done = self.step_count >= self.episode_length
         self.write_action_log(observation, action, reward)
+        self.info['num_steps'] = self.step_count
 
         return observation, reward, is_done, self.info
 
@@ -277,27 +273,37 @@ class RealRobotCubeEnv(gym.GoalEnv):
             self._reset_direct_simulation()
 
         self.step_count = 0
+        if self.num_resets * self.episode_length >= 120*1000:
+            print('Not performing full reset, reached maximum number of resets')
 
         # need to already do one step to get initial observation
         # TODO disable frameskip here?
         observation, reward, _, _ = self.step(self._initial_action)
-        while not any(vel < 0.01 for vel in observation["observation"]["velocity"]):
-            observation, reward, _, _ = self.step(self._initial_action)
+        cur_vel = observation["observation"]["velocity"]
+        cur_pos = observation["observation"]["position"]
+        if self.num_resets != -1:
+            # import pdb; pdb.set_trace()
+            while not all(vel < 0.01 for vel in cur_vel) or \
+                    np.abs(cur_pos - self.default_position).max() >= .05:
+                observation, reward, _, _ = self.step(self._initial_action)
+                cur_vel = observation["observation"]["velocity"]
+                cur_pos = observation["observation"]["position"]
+
+                print("cur vel: {}".format(cur_vel))
+                print("cur pos: {}".format(cur_pos))
+                print("default pos: {}".format(self.default_position))
         return observation
 
     def _reset_platform_frontend(self):
         """Reset the platform frontend."""
         # reset is not really possible
         if self.platform is not None:
-            raise RuntimeError(
-                "Once started, this environment cannot be reset."
-            )
-
-        self.platform = robot_fingers.TriFingerPlatformFrontend()
+            self.num_resets += 1
+        else:
+            self.platform = robot_fingers.TriFingerPlatformFrontend()
 
     def _reset_direct_simulation(self):
         """Reset direct simulation.
-
         With this the env can be used without backend.
         """
         # reset simulation
@@ -321,14 +327,11 @@ class RealRobotCubeEnv(gym.GoalEnv):
 
     def seed(self, seed=None):
         """Sets the seed for this env’s random number generator.
-
         .. note::
-
            Spaces need to be seeded separately.  E.g. if you want to sample
            actions directly from the action space using
            ``env.action_space.sample()`` you can set a seed there using
            ``env.action_space.seed()``.
-
         Returns:
             List of seeds used by this environment.  This environment only uses
             a single seed, so the list contains only one element.
@@ -340,7 +343,7 @@ class RealRobotCubeEnv(gym.GoalEnv):
     def _create_observation(self, t, action):
         robot_observation = self.platform.get_robot_observation(t)
         camera_observation = self.platform.get_camera_observation(t)
-         
+
         observation = {
             "observation": {
                 "position": robot_observation.position,
@@ -353,11 +356,6 @@ class RealRobotCubeEnv(gym.GoalEnv):
                 "position": camera_observation.object_pose.position,
                 "orientation": camera_observation.object_pose.orientation,
             },
-            #"cam0_timestamp": camera_observation.cameras[0].timestamp,
-            #"achieved_goal_filtered": {
-            #    "position": camera_observation.filtered_object_pose.position,
-            #    "orientation": camera_observation.filtered_object_pose.orientation,
-            #},
         }
         return observation
 
@@ -389,7 +387,6 @@ class CubeEnv(RealRobotCubeEnv):
         save_npz: str = None
     ):
         """Initialize.
-
         Args:
             cube_goal_pose (dict): Goal pose for the cube.  Dictionary with
                 keys "position" and "orientation".
@@ -410,5 +407,3 @@ class CubeEnv(RealRobotCubeEnv):
         self.initial_pose = self.initializer.get_initial_state()
         self.goal = self.initializer.get_goal().to_dict()
         return super(CubeEnv, self).reset()
-
-
