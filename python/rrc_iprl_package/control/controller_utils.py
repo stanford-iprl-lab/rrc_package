eff48398ad50352e2d57cf72c06999ad960596c5
ClaireLC <clairech@stanford.edu>
2020-12-06 13:01:11 -0800

    change finger base angles

--- python/rrc_iprl_package/control/controller_utils.py
+++ python/rrc_iprl_package/control/controller_utils.py
@@ -26,9 +26,9 @@ OBJ_MU = 1
 
 # Here, hard code the base position of the fingers (as angle on the arena)
 r = 0.15
-theta_0 = 80
-theta_1 = 310
-theta_2 = 200
+theta_0 = 60
+theta_1 = 300
+theta_2 = 180
 FINGER_BASE_POSITIONS = [
            np.array([[np.cos(theta_0*(np.pi/180))*r, np.sin(theta_0*(np.pi/180))*r, 0]]),
            np.array([[np.cos(theta_1*(np.pi/180))*r, np.sin(theta_1*(np.pi/180))*r, 0]]),
