5ed46a39ac28a0f3b8133fb491e9d577664456a3
Jeff <--global>
2020-12-06 23:53:44 -0500

    tune

--- python/rrc_iprl_package/control/control_policy.py
+++ python/rrc_iprl_package/control/control_policy.py
@@ -57,7 +57,7 @@ class ImpedanceControllerPolicy:
     KP_REPOSE = KP
     KV_REPOSE = KV
 
-    kp_obj = 0.03
+    kp_obj = 0.005
     KP_OBJ = [kp_obj,
               kp_obj,
               kp_obj,
