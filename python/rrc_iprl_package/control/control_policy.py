9a766bc2562a77253cb95ca7273e5670305b4b6b
ClaireLC <clairech@stanford.edu>
2020-12-10 10:15:31 -0800

    increase number of tries

--- python/rrc_iprl_package/control/control_policy.py
+++ python/rrc_iprl_package/control/control_policy.py
@@ -66,7 +66,7 @@ class ImpedanceControllerPolicy:
 
     # Re-orientation constants
     MIN_Z_ERROR = 0.5
-    MAX_Z_TRIES = 2
+    MAX_Z_TRIES = 3
     Z_INCR      = np.pi/3
 
     FT_RADIUS   = 0.0095
